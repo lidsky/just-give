01243c5f659602f8cdc5d227013518bcbca5618c
lidsky <hmr1808@gmail.com>
2014-12-17 22:09:04 -0500

    init repo

---
+++ just-give-server/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+JustGiveServer::Application.config.secret_token = '052de509c1e384872e651230e4acfc429b9ff222bb008aab220495e5c95d921c40665def858e615981aa4a528a73f49a04488e4b735294ae6526fd2d8f1b5681'
