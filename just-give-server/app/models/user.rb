fe8aa634e54f8d923dc825b0787093a919bc950f
lidsky <hmr1808@gmail.com>
2014-12-21 16:55:46 -0500

    add email regex validation, lowercase before saving, hash password

--- just-give-server/app/models/user.rb
+++ just-give-server/app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 5 }
+  before_save { self.email = email.downcase}
+
+  VALID_EMAIL_REGEX = /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/
+
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
+
 end
--- just-give-server/db/schema.rb
+++ just-give-server/db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141221074304) do
+ActiveRecord::Schema.define(version: 20141221214115) do
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
